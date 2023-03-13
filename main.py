19972b20d6ea98fc951f59200e94fc7816e8a3d5
rabboy <legovacca@mail.ru>
2023-03-13 12:54:51 +0300

    checkstatuscode

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("helloworld")
-print("byworld)")
+import requests
+from bs4 import BeautifulSoup as b
+ 
+URL = 'https://youla.ru/moskva?q=lego%20back%20to%20the%20future'
+r = requests.get(URL)
+print(r.status_code)
